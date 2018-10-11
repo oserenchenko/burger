97d5dfb65074745fdcc29a7c9f399d0c36b0c7ce
Olga <oserenchenko@gmail.com>
2018-10-10 22:05:06 -0600

    picture

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,3 +3,7 @@ VALUES ("Chicken Avacado Bacon"), ("Double Cheeseburger"), ("BBQ Cheddar Bacon")
 
 
 SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name) VALUES ("testing");
+
+UPDATE burgers SET devoured = true WHERE id = 1;
